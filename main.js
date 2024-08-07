1d9e33e63dbcd9199c31ea8bc832ee9deab4cf14
hey1mryan <152661883+hey1mryan@users.noreply.github.com>
2024-08-06 22:45:30 -0400

    Commit 1.0

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-// let buttonRef = document.getElementById('generateButton');
-// let catImg = document.getElementById('catImg');
-
-
-// function generateCat(event) {
-//     event.preventDefault();
-
-//     fetch("https://api.thecatapi.com/v1/images/search")
-//     .then(function(response){
-//         console.log(response);
-//         return response.json();
-//     })
-
-//     .then(function(myJson){
-//         // Where we are creating our meaningful code to use the API response
-//         console.log(myJson.message);
-//     })
-
-// }
-
-// buttonRef.addEventListener("click", generateCat);
-
-
-
-
 let buttonRef = document.getElementById('generateButton');
 let catImg = document.getElementById('catImg');
 
